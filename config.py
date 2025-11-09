35241f77856fcfa2ea6eff1e65c4aa0ca4207f2a
Aleksei Nushtaev <nushtaev2110@gmail.com>
2025-11-09 23:30:57 +0500

    fix lang

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ load_dotenv()
 TG_TOKEN: Optional[str] = os.environ.get("TG_TOKEN")
 URL_VPN: Optional[str] = os.environ.get("URL_VPN")
 URL_BOT: Optional[str] = os.environ.get("URL_BOT")
-LANG: Optional[str] = os.environ.get("LANG")
+LANG: Optional[str] = os.environ.get("BOT_LANG")
 ADMIN_IDS: List[int] = [int(x) for x in os.environ.get("ADMIN_IDS", "").split()]
 
